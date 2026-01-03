415583a5b6a9a9b0e5dc0b626f2810c1d5fee6ca
pasantharupathi <pasantharupathi1@gmail.com>
2026-01-04 01:55:40 +0530

    implement the app jsx

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Properties from './pages/Properties';
+import PropertyDetails from './pages/PropertyDetails';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import './styles/main.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="app-container">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/properties" element={<Properties />} />
+          <Route path="/properties/:id" element={<PropertyDetails />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
+
